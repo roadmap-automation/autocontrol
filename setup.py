6d7d9bb55824cc580def849e9d96ea93451f3fe5
criosx <mail@frank-heinrich.net>
2024-05-24 11:23:52 -0400

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 setup(
     name='autocontrol',
     version='0.1',
-    packages=[''],
-    package_dir={'': 'autocontrol'},
+    packages=['autocontrol'],
     url='https://github.com/criosx/autocontrol',
     license='MIT License',
     author='Frank Heinrich',
     author_email='fheinric@andrew.cmu.edu',
-    description='Autocontol task scheduler'
+    description='Autocontol task scheduler',
+    requires=[
+        "numpy", "requests", "flask", "werkzeug", "sqlalchemy", "streamlit", "pandas", "graphviz", "pydantic"
+    ]
 )
